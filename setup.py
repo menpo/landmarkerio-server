a974713264f12dd9a17436e2bc6e86fbd296dc69
James Booth <jabooth@gmail.com>
2014-09-22 10:03:12 +0100

    include conda recipes, modernize setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from os.path import join
+import sys
 import versioneer
 
 project_name = 'landmarkerio'
@@ -11,6 +12,16 @@ versioneer.versionfile_source = '{}/_version.py'.format(project_name)
 versioneer.versionfile_build = '{}/_version.py'.format(project_name)
 versioneer.tag_prefix = 'v'  # tags are like v1.2.0
 versioneer.parentdir_prefix = project_name + '-'  # dirname like 'menpo-v1.2.0'
+
+install_requires = ['menpo>=0.3.0',
+                    'numpy>=1.8.1',
+                    'Flask>=0.10.1',
+                    'Flask-RESTful>=0.2.11',
+                    'CherryPy>=3.5.0',
+                    'joblib>=0.8.2']
+
+if sys.version_info.major == 2:
+    install_requires.extend(['enum>=0.4.4', 'pathlib>=1.0'])
 
 setup(name='landmarkerio',
       version=versioneer.get_version(),
@@ -29,14 +40,7 @@ setup(name='landmarkerio',
           'Programming Language :: Python :: 2.7',
       ],
       packages=['landmarkerio'],
-      install_requires=['menpo>=0.3.0',
-                        'Flask>=0.10.1',
-                        'enum>=0.4.4',
-                        'Flask-RESTful>=0.2.11',
-                        'CherryPy>=3.5.0',
-                        'numpy>=1.8.1',
-                        'pathlib>=1.0',
-                        'joblib>=0.8.2'],
+      install_requires=install_requires,
       scripts=[join('landmarkerio', 'lmio'),
                join('landmarkerio', 'lmioserve'),
                join('landmarkerio', 'lmiocache')]
